3a72b591deea55aa7d176b09cb58dca58c526a8e
unknown <przemek.dudycz@gmail.com>
2023-01-23 21:46:42 +0100

    Finish day11

--- day11/day11.go
+++ day11/day11.go
@@ -13,6 +13,31 @@ type Monkey struct {
 	ChooseMonkey    func(int) int
 }
 
+// Chinease theorem
+var (
+	nums = []int{2, 3, 5, 7, 11, 13, 17, 19}
+	prod = getProd()
+	pp   = getPP()
+	inv  = getNaiveInv()
+)
+
+func getProd() int {
+	result := 1
+	for _, num := range nums {
+		result = result * num
+	}
+	return result
+}
+
+func getPP() []int {
+	pp := []int{}
+	for _, num := range nums {
+		pp = append(pp, prod/num)
+	}
+
+	return pp
+}
+
 var monkeys = [8]*Monkey{
 	{
 		Items:           []int{66, 79},
@@ -50,6 +75,7 @@ var monkeys = [8]*Monkey{
 			if worryLevel%5 == 0 {
 				return 4
 			}
+
 			return 5
 		},
 	},
@@ -86,7 +112,7 @@ var monkeys = [8]*Monkey{
 			return old + 7
 		},
 		ChooseMonkey: func(worryLevel int) int {
-			if worryLevel%17 == 0 {
+			if worryLevel%11 == 0 {
 				return 3
 			}
 			return 4
@@ -152,32 +178,16 @@ func getMonkeyBusiness() int {
 			for _, itemWorryLevel := range monkeys[turn].Items {
 				monkeys[turn].InspectionCount++
 				worryLevel := monkeys[turn].Operation(itemWorryLevel)
+				worryLevel = worryLevel % prod
 				newMonkey := monkeys[turn].ChooseMonkey(worryLevel)
 				monkeys[turn].Items = monkeys[turn].Items[1:]
 				monkeys[newMonkey].Items = append(monkeys[newMonkey].Items, worryLevel)
 			}
 		}
-
-		for _, monkey := range monkeys {
-			if len(monkey.Items) > 0 {
-				isEven := true
-				for _, items := range monkey.Items {
-					if items%2 != 0 {
-						isEven = false
-						break
-					}
-				}
-
-				if isEven {
-					for index, _ := range monkey.Items {
-						monkey.Items[index] = monkey.Items[index] / 2
-					}
-				}
-			}
-		}
 	}
 
 	printMonkeys()
+
 	mostActives := getTwoMostActive(monkeys)
 
 	return monkeys[mostActives[0]].InspectionCount * monkeys[mostActives[1]].InspectionCount
@@ -200,6 +210,72 @@ func getTwoMostActive(monkeys [8]*Monkey) [2]int {
 	return mostActives
 }
 
+func naiveChineaseTheorem(worryLevel int) int {
+	var rem []int
+
+	for _, num := range nums {
+		rem = append(rem, worryLevel%num)
+	}
+
+	result := 1
+	for {
+		flag := true
+
+		for i := 0; i < len(nums); i++ {
+			if result%nums[i] != rem[i] {
+				flag = false
+				break
+			}
+		}
+
+		if flag {
+			return result
+		}
+		result++
+	}
+}
+
+func gcd(a, b int) int {
+	for b != 0 {
+		t := b
+		b = a % b
+		a = t
+	}
+	return a
+}
+
+func getNaiveInv() []int {
+	inv := []int{}
+	for i, num := range nums {
+		t := pp[i] % num
+
+		result := 2
+		for {
+			if (result*pp[i])%num == t {
+				inv = append(inv, result)
+				break
+			}
+			result += 1
+		}
+	}
+	return inv
+}
+
+func inverseChineaseTheorem(worryLevel int) int {
+	var rem []int
+
+	for _, num := range nums {
+		rem = append(rem, worryLevel%num)
+	}
+
+	t := 0
+	for i, r := range rem {
+		t += r * pp[i] * inv[i]
+	}
+
+	return t % prod
+}
+
 func printMonkeys() {
 	fmt.Printf("%v\n", "-----------------------------------")
 	for _, monkey := range monkeys {
@@ -212,26 +288,34 @@ func printMonkeys() {
 	fmt.Printf("%v\n", "-----------------------------------")
 }
 
-func findGVD(monkey int) int {
-	gvds := []int{2, 3, 5, 7, 13}
-	invalid := map[int]bool{}
+func test() int {
+	tnum := []int{3, 4, 5}
+	trem := []int{2, 3, 1}
+	tprod := 60
 
-	for _, gvd := range gvds {
-		for _, worryLevel := range monkeys[monkey].Items {
-			if worryLevel%gvd != 0 {
-				invalid[gvd] = true
+	tpp := []int{}
+	for _, num := range tnum {
+		tpp = append(tpp, tprod/num)
+	}
+
+	tinv := []int{}
+	for i, num := range tnum {
+		t := tpp[i] % num
+
+		result := 2
+		for {
+			if (result*tpp[i])%num == t {
+				inv = append(tinv, result)
 				break
 			}
-		}
-		if !invalid[gvd] {
-			return gvd
+			result += 1
 		}
 	}
-	return -1
-}
 
-func reduceMonkeyByGVD(monkey int, gvd int) {
-	for i := 0; i < len(monkeys[monkey].Items); i++ {
-		monkeys[monkey].Items[i] = monkeys[monkey].Items[i] / gvd
+	t := 0
+	for i, r := range trem {
+		t += r * tpp[i] * tinv[i]
 	}
+
+	return t % tprod
 }
